8d4c8d1d39716dec020421492a268f96f3ec0d0e
huangjianqin <18814127639@163.com>
2021-11-28 22:37:07 +0800

    Hessian2序列化优化

---
+++ kin-serialization-api/src/main/java/org/kin/serialization/OutputStreams.java
@@ -0,0 +1,43 @@
+package org.kin.serialization;
+
+import org.kin.framework.concurrent.FastThreadLocal;
+import org.kin.framework.utils.reflection.FieldUpdaters;
+import org.kin.framework.utils.reflection.ReferenceFieldUpdater;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * @author huangjianqin
+ * @date 2021/11/28
+ */
+public final class OutputStreams {
+    private static final ReferenceFieldUpdater<ByteArrayOutputStream, byte[]> OUTPUT_STREAM_BUF_UPDATER =
+            FieldUpdaters.newReferenceFieldUpdater(ByteArrayOutputStream.class, "buf");
+
+    /**
+     * 复用{@link ByteArrayOutputStream}中的 byte[]
+     */
+    private static final FastThreadLocal<ByteArrayOutputStream> OUTPUT_STREAM_FAST_THREAD_LOCAL = new FastThreadLocal<ByteArrayOutputStream>() {
+
+        @Override
+        protected ByteArrayOutputStream initialValue() {
+            return new ByteArrayOutputStream(Serialization.DEFAULT_BUF_SIZE);
+        }
+    };
+
+    public static ByteArrayOutputStream getByteArrayOutputStream() {
+        return OUTPUT_STREAM_FAST_THREAD_LOCAL.get();
+    }
+
+    public static void resetBuf(ByteArrayOutputStream buf) {
+        // for reuse
+        buf.reset();
+
+        // 防止hold过大的内存块一直不释放
+        if (OUTPUT_STREAM_BUF_UPDATER.get(buf).length > Serialization.MAX_CACHED_BUF_SIZE) {
+            OUTPUT_STREAM_BUF_UPDATER.set(buf, new byte[Serialization.DEFAULT_BUF_SIZE]);
+        }
+    }
+
+    private OutputStreams() {}
+}
--- kin-serialization-hessian2/src/main/java/org/kin/serialization/hessian2/Hessian2Serialization.java
+++ kin-serialization-hessian2/src/main/java/org/kin/serialization/hessian2/Hessian2Serialization.java
@@ -2,8 +2,15 @@ package org.kin.serialization.hessian2;
 
 import com.caucho.hessian.io.Hessian2Input;
 import com.caucho.hessian.io.Hessian2Output;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
+import org.kin.framework.io.ByteBufferInputStream;
+import org.kin.framework.io.ByteBufferOutputStream;
+import org.kin.framework.io.ExpandableByteBufferOutputStream;
 import org.kin.framework.utils.ExceptionUtils;
 import org.kin.framework.utils.Extension;
+import org.kin.serialization.OutputStreams;
 import org.kin.serialization.Serialization;
 import org.kin.serialization.SerializationType;
 
@@ -11,6 +18,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.ByteBuffer;
 
 /**
  * Created by 健勤 on 2017/2/9.
@@ -24,25 +32,47 @@ public class Hessian2Serialization implements Serialization {
             throw new NullPointerException("Serialized object must be not null");
         }
 
-
         if (!(target instanceof Serializable)) {
             throw new IllegalStateException("Serialized class " + target.getClass().getSimpleName() + " must implement java.io.Serializable");
         }
 
-        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            Hessian2Output hessian2Output = new Hessian2Output(outputStream);
-            hessian2Output.writeObject(target);
+        ByteArrayOutputStream baos = OutputStreams.getByteArrayOutputStream();
+        try {
+            serialize0(new Hessian2Output(baos), target);
+            return baos.toByteArray();
+        }finally {
+            OutputStreams.resetBuf(baos);
+        }
+    }
 
-            hessian2Output.close();
-            return outputStream.toByteArray();
+    @Override
+    public <T> ByteBuffer serialize(ByteBuffer byteBuffer, T target) {
+        ExpandableByteBufferOutputStream bbos = new ExpandableByteBufferOutputStream(byteBuffer);
+        serialize0(new Hessian2Output(bbos), target);
+        return bbos.getSink();
+    }
+
+    @Override
+    public <T> void serialize(ByteBuf byteBuf, T target) {
+        serialize0(new Hessian2Output(new ByteBufOutputStream(byteBuf)), target);
+    }
+
+    private <T> void serialize0(Hessian2Output output, T target){
+        try{
+            output.writeObject(target);
+            output.flush();
         } catch (IOException e) {
             ExceptionUtils.throwExt(e);
         }
-        //理论上不会到这里
-        throw new IllegalStateException("encounter unknown error");
+        finally {
+            try {
+                output.close();
+            } catch (IOException e) {
+                ExceptionUtils.throwExt(e);
+            }
+        }
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public <T> T deserialize(byte[] bytes, Class<T> targetClass) {
         if (bytes == null || bytes.length <= 0) {
@@ -50,13 +80,36 @@ public class Hessian2Serialization implements Serialization {
         }
 
         try (ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes)) {
-            Hessian2Input hessian2Input = new Hessian2Input(inputStream);
-            T ret = (T) hessian2Input.readObject(targetClass);
-            hessian2Input.close();
-
-            return ret;
+            return deserialize0(new Hessian2Input(inputStream), targetClass);
         } catch (IOException e) {
             ExceptionUtils.throwExt(e);
+        }
+        //理论上不会到这里
+        throw new IllegalStateException("encounter unknown error");
+    }
+
+    @Override
+    public <T> T deserialize(ByteBuffer byteBuffer, Class<T> targetClass) {
+        return deserialize0(new Hessian2Input(new ByteBufferInputStream(byteBuffer)), targetClass);
+    }
+
+    @Override
+    public <T> T deserialize(ByteBuf byteBuf, Class<T> targetClass) {
+        return deserialize0(new Hessian2Input(new ByteBufInputStream(byteBuf)), targetClass);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T deserialize0(Hessian2Input hessian2Input, Class<T> targetClass){
+        try{
+            return (T) hessian2Input.readObject(targetClass);
+        } catch (IOException e) {
+            ExceptionUtils.throwExt(e);
+        } finally {
+            try {
+                hessian2Input.close();
+            } catch (IOException e) {
+                ExceptionUtils.throwExt(e);
+            }
         }
         //理论上不会到这里
         throw new IllegalStateException("encounter unknown error");
