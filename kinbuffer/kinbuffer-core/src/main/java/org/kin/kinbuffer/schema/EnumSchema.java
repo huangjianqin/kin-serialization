10b701ac7df260f0367a08e0cf850f89b25539f0
huangjianqin <18814127639@163.com>
2021-12-22 00:01:54 +0800

    EnumSchema默认序列化成enum序号以减少序列化bytes

--- kinbuffer/kinbuffer-core/src/main/java/org/kin/kinbuffer/schema/EnumSchema.java
+++ kinbuffer/kinbuffer-core/src/main/java/org/kin/kinbuffer/schema/EnumSchema.java
@@ -1,32 +1,80 @@
 package org.kin.kinbuffer.schema;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableMap;
 import org.kin.kinbuffer.io.Input;
 import org.kin.kinbuffer.io.Output;
 
-import java.util.Objects;
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
+ * enum schema
+ *
  * @author huangjianqin
  * @date 2021/12/20
  */
-public class EnumSchema<E extends Enum<E>> implements PolymorphicSchema{
+public class EnumSchema<E extends Enum<E>> implements PolymorphicSchema {
+    /** enum class */
     private final Class<E> enumClass;
+    /** 是否将enum序列化成enum ordinal, 否则序列化成enum name */
+    private final boolean enumByOrdinal;
+    /** key -> enum name , value -> enum instance */
+    private final BiMap<String, E> name2Inst;
+    /** key -> enum ordinal , value -> enum instance */
+    private final BiMap<Integer, E> id2Inst;
 
-    public EnumSchema(Class<E> enumClass) {
+    public EnumSchema(Class<E> enumClass){
+        this(enumClass, true);
+    }
+
+    /**
+     * @param enumByOrdinal 是否将enum序列化成enum ordinal, 否则序列化成enum name
+     */
+    public EnumSchema(Class<E> enumClass, boolean enumByOrdinal) {
         this.enumClass = enumClass;
+        this.enumByOrdinal = enumByOrdinal;
+
+        ImmutableBiMap.Builder<String, E> name2InstBuilder = ImmutableBiMap.builder();
+        ImmutableBiMap.Builder<Integer, E> id2InstBuilder = ImmutableBiMap.builder();
+        for (E instance : enumClass.getEnumConstants()) {
+            id2InstBuilder.put(instance.ordinal(), instance);
+            name2InstBuilder.put(instance.name(), instance);
+        }
+
+        name2Inst = name2InstBuilder.build();
+        id2Inst = id2InstBuilder.build();
     }
 
     @Override
     public Object read(Input input) {
-        //enum name
-        String name = input.readString();
-        return Enum.valueOf(enumClass, name);
+        if(enumByOrdinal){
+            //enum id
+            int enumId = input.readInt();
+            return id2Inst.get(enumId);
+        }
+        else {
+            //enum name
+            String name = input.readString();
+            return Enum.valueOf(enumClass, name);
+        }
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public void write(Output output, Object o) {
-        String enumName = ((Enum<E>) o).name();
-        output.writeString(enumName);
+        if(enumByOrdinal){
+            //enum id
+            int enumId = id2Inst.inverse().get((E)o);
+            output.writeInt(enumId);
+        }
+        else {
+            //enum name
+            String enumName = ((Enum<E>) o).name();
+            output.writeString(enumName);
+        }
     }
 }
