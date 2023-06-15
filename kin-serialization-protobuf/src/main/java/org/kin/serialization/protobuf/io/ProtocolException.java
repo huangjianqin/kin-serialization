acb81a6e9771bfa07c12e8b19ff22ab23385c637
huangjianqin <459564034@qq.com>
2023-06-15 23:35:36 +0800

    异常描述修改

--- kin-serialization-protobuf/src/main/java/org/kin/serialization/protobuf/io/ProtocolException.java
+++ kin-serialization-protobuf/src/main/java/org/kin/serialization/protobuf/io/ProtocolException.java
@@ -21,34 +21,34 @@ public final class ProtocolException extends ProtobufException {
 
     static ProtocolException misreportedSize() {
         return new ProtocolException(
-                "CodedInput encountered an embedded string or bytes " +
+                "Input encountered an embedded string or bytes " +
                         "that misreported its size.");
     }
 
     static ProtocolException negativeSize() {
         return new ProtocolException(
-                "CodedInput encountered an embedded string or message " +
+                "Input encountered an embedded string or message " +
                         "which claimed to have negative size.");
     }
 
     static ProtocolException malformedVarInt() {
         return new ProtocolException(
-                "CodedInput encountered a malformed varint.");
+                "Input encountered a malformed varint.");
     }
 
     static ProtocolException invalidTag() {
         return new ProtocolException(
-                "Protocol message contained an invalid tag (zero).");
+                "protocol message contained an invalid tag (zero).");
     }
 
     static ProtocolException invalidEndTag() {
         return new ProtocolException(
-                "Protocol message end-group tag did not match expected tag.");
+                "protocol message end-group tag did not match expected tag.");
     }
 
     static ProtocolException invalidWireType() {
         return new ProtocolException(
-                "Protocol message tag had invalid wire type.");
+                "protocol message tag had invalid wire type.");
     }
 }
 
